2a4d181df674969782711a69c663447a81e762f2
Samar Hayat <samarhayat.dev@gmail.com>
2024-12-17 20:35:06 +0500

    update

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -3,7 +3,7 @@ import { setContext } from "@apollo/client/link/context";
 import { nhost } from "./nhost";
 
 const httpLink = createHttpLink({
-  uri: "https://ngnpvbdevhrykamgfomm.graphql.ap-south-1.nhost.run/v1",
+  uri: process.env.NEXT_PUBLIC_NHOST_GRAPHQL_URL,
 });
 
 const authLink = setContext(async (_: any, { headers }: any) => {
